29e73a7eeb10124d636eb71c3a609b67128fc252
Kame-Bashuna <basshunakameroba@gmail.com>
2024-07-15 23:08:40 +0300

    add file

--- api2/serializers.py
+++ api2/serializers.py
@@ -6,3 +6,11 @@ class Class_PeriodSerializer(serializers.ModelSerializer):
     class Meta:
         model=Class_Period
         field="__all__"
+from rest_framework import serializers
+from class_period.models import Class_Period
+
+
+class Class_PeriodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Class_Period
+        field="__all__"
