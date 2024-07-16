d7394b0f05d907e6607e597a8abd5c026401a4b2
Kame-Bashuna <basshunakameroba@gmail.com>
2024-07-16 07:42:32 +0300

    add file

--- api2/views.py
+++ api2/views.py
@@ -4,6 +4,12 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from class_period.models import Class_Period
 from .serializers import Class_PeriodListView
+from student.models import Student
+from .serializers import StudentListView
+from teacher.models import Teacher
+from .serializers import TeacherListView
+from course.models import Course
+from .serializers import CourseListView
 from rest_framework.response import Response
 
 
@@ -12,5 +18,28 @@ class Class_PeriodListView(APIView):
         class_period=Class_Period.objects.all()
         serializer=Class_PeriodSerializer(class_period, many=True)
         return Response(serializer.data)
+
+
+
+class StudentListView(APIView):
+    def get (self,request):
+        student=Student.objects.all()
+        serializer=StudentSerializer(student, many=True)
+        return Response(serializer.data)
+
+class TeacherListView(APIView):
+    def get (self,request):
+        student=Student.objects.all()
+        serializer=TeacherSerializer(teacher, many=True)
+        return Response(serializer.data) 
+
+
+class CourseListView(APIView):
+    def get (self,request):
+        student=Student.objects.all()
+        serializer=CourseSerializer(course, many=True)
+        return Response(serializer.data)   
+
+                  
 
 # Create your views here.
