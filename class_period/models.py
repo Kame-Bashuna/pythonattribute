d3552aa8ed0aa4078c754ec51bc554e65b237977
Kame-Bashuna <basshunakameroba@gmail.com>
2024-07-25 07:15:51 +0300

    add file

--- class_period/models.py
+++ class_period/models.py
@@ -1,19 +1,4 @@
-# from django.db import models
 
-
-# class Class_Period():
-#     start_time=models.TimeField()
-#     end_time=models.TimeField()
-#     course=models.CharField(max_length=100)
-#     classroom=models.CharField(max_length=100)
-#     day_of_the_week=models.CharField(max_length=100)
-    
-
-
-#     def __str__(self):
-#         return f"{self.course} {self.start_time}"
-
-# # Create your models here.
 
 
 from django.db import models
